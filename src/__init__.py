51ed5b7b3131da4eba3ced3b2e2451a3f294ae68
Pearl Sortman <pearlsortman@gmail.com>
2018-01-21 02:57:27 -0500

    Added comments

--- src/__init__.py
+++ src/__init__.py
@@ -3,10 +3,15 @@ import json
 import requests
 from bs4 import BeautifulSoup
 
+##
+# STEP 1: Scrape SpaceFinderDC for space contact data
+##
+
 baseUrl = 'https://dc.spacefinder.org'
 url = baseUrl + '/spaces.html?page=1'
 completeSpaceList = []
 
+# While there is a valid Next Page link, continue to next page of results
 while url:
     print(url)
     response = requests.get(url)
@@ -15,24 +20,35 @@ while url:
     if status != 200:
         print('Request error: {}'.format(status))
 
+    # Successful 200 response from SpaceFinder.org
     else:
         html = response.content
         soup = BeautifulSoup(html, 'html.parser')
         spaces = soup.find_all('div', class_='detail')
         print('This page contains {} results.'.format(len(spaces)))
 
+        # For each space in search results list, navigate to it's detailed view and parse
         for s in spaces:
+            # Get the space details url and setup with BeautifulSoup for html parsing
             spaceUrl = baseUrl + s.find('a')['href']
             spaceResponse = requests.get(spaceUrl)
             spaceHtml = spaceResponse.content
             bs = BeautifulSoup(spaceHtml, 'html.parser')
+
             name = bs.find('h1').string
             contactInfo = bs.find_all('span', class_='info_pair')
+
+            # This is a hack because the SpaceFinder site blows and html structure varies slightly on some pages
+            # * insert massive eye roll emoji here *
+            # Any rows in the final csv with ERROR or unexpected data, please verify manually
             if len(contactInfo) < 5:
                 website = 'ERROR'
                 person = 'ERROR'
                 phone = 'ERROR'
                 email = 'ERROR'
+
+            # Even if the contact info section has the expected number of elements, they could be in a bizarre order
+            # This handles those cases to avoid exceptions.. sloppily I admit
             else:
                 website = contactInfo[0].find('a')
                 if website:
@@ -46,7 +62,10 @@ while url:
                 email = contactInfo[3].find('a')
                 if email:
                     email = email.text
+
             lastUpdated = bs.find('p', class_='subtle').text
+
+            # Format the json object for our space and add to the main list
             space = {
                 "Space Name": name,
                 "Website": website,
@@ -56,11 +75,15 @@ while url:
             }
             completeSpaceList.append(space)
 
+        # Reset url and check for the url of the next page
+        url = ''
         nextPage = soup.find(class_='next_page')
         if 'disabled' not in nextPage.attrs['class']:
             url = baseUrl + nextPage['href']
-        else:
-            url = ''
+
+##
+# STEP 2: Format and write scraped json list into CSV document
+##
 
 completeSpaceData = {
     "spaces": completeSpaceList
@@ -69,10 +92,11 @@ dump = json.dumps(completeSpaceData)
 spaceListParsed = json.loads(dump)
 spaceData = spaceListParsed['spaces']
 
-# open a file for writing
-spaceDataCsv = open('/Users/i851870/Code/personal/hoppispace/SpaceFinderDCContactList.csv', 'w')
+# Open a file for writing
+tempFile = '/Users/i851870/Code/personal/hoppispace/SpaceFinderDCContactList.csv'
+spaceDataCsv = open(tempFile, 'w')
 
-# create the csv writer object
+# Create the csv writer object
 csvwriter = csv.writer(spaceDataCsv)
 
 count = 0
@@ -84,4 +108,5 @@ for space in spaceData:
 
     csvwriter.writerow(space.values())
 
+# Close/save
 spaceDataCsv.close()
